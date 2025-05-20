4cb7e16e7c596ad0eeca625f062aa487da0da0a9
jedaqsaul <jedaqsaul@gmail.com>
2025-05-20 19:34:23 +0300

    comples the instance-methods lab

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print('Hello World!')
+    def walk(self):
+        print('The person is walking.')
 
-    #Instance method definition
-    pass
+person_1=Person()
+person_1.talk()
+person_1.walk()
